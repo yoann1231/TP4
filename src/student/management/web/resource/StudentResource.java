7e8c4487a48b2436f5ee54003a5b54b8a7d59eb1
yvanyann <1982226034@qq.com>
2024-01-13 15:47:11 +0800

    格式化代码

--- src/student/management/web/resource/StudentResource.java
+++ src/student/management/web/resource/StudentResource.java
@@ -1,48 +1,55 @@
-package student.management.web.resource; 
+package student.management.web.resource;
+
 import java.net.*;
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
+
 import student.management.web.data.*;
 import student.management.web.service.*;
+
 @Path("/students")
 public class StudentResource {
-StudentService service = new StudentService();
-@Context
-UriInfo uriInfo;
-@POST
-@Consumes(MediaType.APPLICATION_XML)
-@Produces(MediaType.APPLICATION_XML)
-public Response addStudent(Student s) {
-Student student = service.addStudent(s);
-if(student == null) {
-return Response.status(Response.Status.BAD_REQUEST).build();
-}
-URI uri = uriInfo.getRequestUri();
-String newUri = uri.getPath() + "/" + student.getId();
-return Response.status(Response.Status.CREATED)
-.entity(student)
-.contentLocation(uri.resolve(newUri))
-.build();
-}
-@DELETE
-@Path("/{id}")
-@Produces(MediaType.APPLICATION_XML)
-public Response deleteStudent(@PathParam("id") int id) {
-if(service.deleteStudent(id) == false) {
-return Response.status(Response.Status.NOT_FOUND).build();
-}
-return Response.status(Response.Status.OK).build();
-}
-@GET
-@Path("/{id}")
-@Produces(MediaType.APPLICATION_XML)
-public Response getStudent(@PathParam("id") int id) {
-    Student student = service.getStudent(id);
-    if (student == null) {
-        return Response.status(Response.Status.NOT_FOUND).build();
+    StudentService service = new StudentService();
+    @Context
+    UriInfo uriInfo;
+
+    @POST
+    @Consumes(MediaType.APPLICATION_XML)
+    @Produces(MediaType.APPLICATION_XML)
+    public Response addStudent(Student s) {
+        Student student = service.addStudent(s);
+        if (student == null) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        URI uri = uriInfo.getRequestUri();
+        String newUri = uri.getPath() + "/" + student.getId();
+        return Response.status(Response.Status.CREATED)
+                .entity(student)
+                .contentLocation(uri.resolve(newUri))
+                .build();
     }
-    return Response.status(Response.Status.OK).entity(student).build();
-}
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response deleteStudent(@PathParam("id") int id) {
+        if (service.deleteStudent(id) == false) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.status(Response.Status.OK).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_XML)
+    public Response getStudent(@PathParam("id") int id) {
+        Student student = service.getStudent(id);
+        if (student == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        return Response.status(Response.Status.OK).entity(student).build();
+    }
+
     @GET
     @Path("/{id1}")
     @Produces(MediaType.APPLICATION_XML)
--- src/student/management/web/service/StudentService.java
+++ src/student/management/web/service/StudentService.java
@@ -1,33 +1,40 @@
-package student.management.web.service; 
+package student.management.web.service;
+
 import java.util.*;
+
 import student.management.web.data.*;
+
 public class StudentService {
-private static Map<Integer, Student> STUDENT_DATA = new HashMap<Integer, Student>();
-private int getNewId() {
-int newId = 0;
-for (int id : STUDENT_DATA.keySet()) {
-if (newId < id)
-newId = id;
-}
-return ++newId;
-}
-public Student addStudent(Student s) {
-int id = getNewId();
-if(STUDENT_DATA.get(s.getId()) != null) {
-return null;
-}
-s.setId(id);
-STUDENT_DATA.put(id, s);
-return s;
-}
-public boolean deleteStudent2(int id) {
-if(STUDENT_DATA.get(id) == null) {
-return false;
-}
-STUDENT_DATA.remove(id);
-return true;
-}
-public Student getStudent(int id) {
-return STUDENT_DATA.get(id);
-}
+    private static Map<Integer, Student> STUDENT_DATA = new HashMap<Integer, Student>();
+
+    private int getNewId() {
+        int newId = 0;
+        for (int id : STUDENT_DATA.keySet()) {
+            if (newId < id)
+                newId = id;
+        }
+        return ++newId;
+    }
+
+    public Student addStudent(Student s) {
+        int id = getNewId();
+        if (STUDENT_DATA.get(s.getId()) != null) {
+            return null;
+        }
+        s.setId(id);
+        STUDENT_DATA.put(id, s);
+        return s;
+    }
+
+    public boolean deleteStudent2(int id) {
+        if (STUDENT_DATA.get(id) == null) {
+            return false;
+        }
+        STUDENT_DATA.remove(id);
+        return true;
+    }
+
+    public Student getStudent(int id) {
+        return STUDENT_DATA.get(id);
+    }
 }
